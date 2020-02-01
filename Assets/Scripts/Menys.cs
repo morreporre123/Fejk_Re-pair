7a73779fcc414bc4196a5b3b1d65cec2678ea8ff
morreporre123 <mauritz.backman@learnet.se>
2020-02-01 16:20:53 +0100

    saker wioooo

---
+++ Assets/Scripts/Menys.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menys : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
